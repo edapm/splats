9ec0422bdde421293a176698d3cb08e2b0e74470
Ben Robinson <ben@txrx.org.uk>
2016-08-06 20:13:48 +0100

    Add some react boilerplate

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Root from "./components/Root.jsx";
+
+export default function(rootDomElement) {
+    ReactDOM.render(<Root/>, rootDomElement)
+}
