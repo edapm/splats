0befe9fa0f416d03d43c780d639c7326b9f1cab6
Ben Robinson <ben@txrx.org.uk>
2016-08-14 21:35:03 +0100

    Started migrating to async/await patterns because fun

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1,13 +1,15 @@
+import "babel-polyfill";
 import app from "./app.js";
+import { get } from "./utils.js";
 
-const req = new XMLHttpRequest();
-req.addEventListener("load", () => {
-    const leaders = JSON.parse(req.responseText);
+async function main() {
+    const response = await get("/api/leaders");
+    const leaders = JSON.parse(response);
     app({
         rootDomElement: document.getElementById("root"),
         leaders,
         windowDevTools: window.devToolsExtension ? window.devToolsExtension() : f => f,
     });
-});
-req.open("GET", "/api/leaders");
-req.send();
+}
+
+main();
