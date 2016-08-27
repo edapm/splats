6dbf555144afe579d9833fe27953e5899679f2ad
Ben Robinson <ben@txrx.org.uk>
2016-08-27 11:11:05 -0400

    Leaders faces are now constant aspect ratios

--- assets/js/components/SplatElement.jsx
+++ assets/js/components/SplatElement.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 export default ({ name, image, vote }) => (
     <div className="splatelement" onClick={vote}>
-        <div
-            className="splatelement-image"
-            style={{ backgroundImage: `url(${image})` }}
-            >
-            &nbsp;
+        <div className="splatelement-content">
+            <div
+                className="splatelement-image"
+                style={{ backgroundImage: `url(${image})` }}
+                >
+                &nbsp;
+            </div>
+            <div className="splatelement-text">{name}</div>
+            <div className="splatelement-dimlayer" />
         </div>
-        <div className="splatelement-text">{name}</div>
-        <div className="splatelement-dimlayer" />
     </div>
 );
