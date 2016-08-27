ad80fd66aa3bdb31c3c5beba094cf529d96c15be
Ben Robinson <ben@txrx.org.uk>
2016-08-27 13:57:15 -0400

    Add temporary hacky way of doing JS build to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const exec = require("child_process").exec;
 
 const HTML_PATHS = ["./assets/html/**/*.html"];
 const IMG_PATHS = ["./assets/images/*.jpg", "./data/images/*.{jpg,JPG}"];
+const JS_PATHS = ["./assets/js/**/*.{js,jsx}"];
 const SCSS_PATHS = ["./assets/scss/**/*.scss"];
 
 gulp.task("copy-html", () =>
@@ -19,11 +21,18 @@ gulp.task("build-scss", () =>
         .pipe(gulp.dest("./static/css"))
 );
 
-gulp.task("build", ["build-scss", "copy-html", "copy-images"]);
+const buildJSCommand = "browserify assets/js/bootstrap.js -t [ babelify --presets [ react es2015 stage-3 ] --plugins [ transform-runtime ] ] --debug -o static/js/bootstrap.js";
+
+gulp.task("build-js", (cb) =>
+    exec(buildJSCommand, cb)
+);
+
+gulp.task("build", ["build-scss", "copy-html", "copy-images", "build-js"]);
 
 gulp.task("default", ["build"]);
 
 gulp.task("watch", ["build"], () => {
     gulp.watch(SCSS_PATHS, ["build-scss"]);
     gulp.watch(HTML_PATHS, ["copy-html"]);
+    gulp.watch(JS_PATHS, ["build-js"]);
 });
