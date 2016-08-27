a6a348b6c26edd8fbab803f00847cca6ef8f563b
Ben Robinson <ben@txrx.org.uk>
2016-08-27 13:08:31 -0400

    ES6-ified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,18 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
 
-var SCSS_PATHS = ["./assets/scss/**/*.scss"];
-var JS_PATHS = ["./assets/js/**/*.js"];
+const SCSS_PATHS = ["./assets/scss/**/*.scss"];
 
-gulp.task("build-scss", function() {
-    return gulp.src(SCSS_PATHS)
+gulp.task("build-scss", () => (
+    gulp.src(SCSS_PATHS)
         .pipe(sass().on("error", sass.logError))
-        .pipe(gulp.dest("./static/css"));
-});
+        .pipe(gulp.dest("./static/css"))
+));
 
 gulp.task("build", ["build-scss"]);
 
 gulp.task("default", ["build"]);
 
-gulp.task("watch", ["build"], function() {
+gulp.task("watch", ["build"], () => {
     gulp.watch(SCSS_PATHS, ["build-scss"]);
 });
