6b935efe280a251f9730c6c992725c7a6459cbc5
Ben Robinson <ben@txrx.org.uk>
2016-08-26 23:20:21 -0400

    Got csv parsing going

--- scripts/populate.py
+++ scripts/populate.py
@@ -1,10 +1,31 @@
 import csv
 import json
 
-# Load file
 
-# Parse csv
+def main():
+    result = []
 
-# Save to leaders.json
+    for row in read_csv():
+        image = row[1]
+        if image == "--":
+            image = None
+        entry = {"name": row[0], "img": image}
+        result.append(entry)
 
-# Wipe images directory, insert into images directory
+    # Save to leaders.json
+    with open('data/leaders.json', 'w') as jsonfile:
+        json.dump(result, jsonfile, indent=4, sort_keys=True)
+
+
+def read_csv():
+    # Load/parse file
+    with open('dump/leaders.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+
+        for i, row in enumerate(reader):
+            if i != 0:
+                yield row
+
+
+if __name__ == '__main__':
+    main()
