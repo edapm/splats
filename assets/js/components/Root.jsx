c7232847e16fe5118bc1c77762f0538dff66557d
Ben Robinson <ben@txrx.org.uk>
2016-08-27 11:22:45 -0400

    Background no longer stretches, doesn't scroll

--- assets/js/components/Root.jsx
+++ assets/js/components/Root.jsx
@@ -6,12 +6,15 @@ import { dialogCancelPressed } from "../actions";
 
 const Root = ({ isDialogVisible, dismissDialog }) => (
     <div className="root">
+        <div className="root-background">&nbsp;</div>
         <div className="root-voteconfirm" hidden={!isDialogVisible}>
             <VoteConfirm onClick={dismissDialog} />
         </div>
-        <h1>Splat a leader!</h1>
-        <p>Click a leader to vote for them!</p>
-        <SplatGrid className="root-splatgrid" />
+        <div className="root-content">
+            <h1>Splat a leader!</h1>
+            <p>Click a leader to vote for them!</p>
+            <SplatGrid className="root-splatgrid" />
+        </div>
     </div>
 );
 
